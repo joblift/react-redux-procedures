ed5b2263000827ffebea2a2bf8b4c8953048a804
remmy hume <rem@remics.net>
2017-12-12 20:28:15 +0100

    Made use of selectors and memoization for better performance

--- src/connectProcedures.js
+++ src/connectProcedures.js
@@ -1,39 +1,71 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
+const getDispatch = dispatch => dispatch;
+const getOwnProps = (_, ownProps) => ownProps;
 
 function buildMapStateToProps(mapStateToProps, mapProceduresToProps) {
-    return (state, ownProps) => ({
-        ...mapStateToProps(state, ownProps),
-        __procedureState: Object.keys(mapProceduresToProps).reduce(
-            (all, p) => ({
-                ...all,
-                [p]: mapProceduresToProps[p].mapStateToProcState
-                    ? mapProceduresToProps[p].mapStateToProcState(state)
-                    : undefined,
-            }),
-            {}
-        ),
-    });
+    // not sure if we can trust .keys() and .values() to be in the same order so we do it with a weird reduce.
+    // not heavy because it's only called once per connectProcedures.
+    const procedureSelectors = Object.keys(mapProceduresToProps).reduce(
+        (ks, key) => ({
+            keys: [...ks.keys, key],
+            selectors: [...ks.selectors, mapProceduresToProps[key].mapStateToProcState || (() => undefined)],
+        }),
+        {
+            keys: [],
+            selectors: [],
+        }
+    );
+
+    return () =>
+        createSelector([mapStateToProps, ...procedureSelectors.selectors], (ownStateProps, ...procedureStates) => ({
+            ...ownStateProps,
+            __procedureState: procedureSelectors.keys.reduce(
+                (all, p, i) => ({
+                    ...all,
+                    [p]: procedureStates[i],
+                }),
+                {}
+            ),
+        }));
 }
 
-function buildMapDispatchToProps(mapDispatchToProps, mapProceduresToProps) {
-    return (dispatch, ownProps) => ({
-        ...(typeof mapDispatchToProps === 'function'
-            ? mapDispatchToProps(dispatch, ownProps)
-            : Object.keys(mapDispatchToProps).reduce(
-                  (all, d) => ({
-                      ...all,
-                      [d]: (...args) => dispatch(mapDispatchToProps[d](...args)),
-                  }),
-                  {}
-              )),
-        ...Object.keys(mapProceduresToProps).reduce(
+const buildGetOwnDispatchProps = mapDispatchToProps =>
+    createSelector(
+        [getDispatch, getOwnProps],
+        (dispatch, ownProps) =>
+            typeof mapDispatchToProps === 'function'
+                ? mapDispatchToProps(dispatch, ownProps)
+                : Object.keys(mapDispatchToProps).reduce(
+                      (all, d) => ({
+                          ...all,
+                          [d]: (...args) => dispatch(mapDispatchToProps[d](...args)),
+                      }),
+                      {}
+                  )
+    );
+
+const buildGetDispatchProcedureProps = mapProceduresToProps =>
+    createSelector(getDispatch, dispatch =>
+        Object.keys(mapProceduresToProps).reduce(
             (all, p) => ({
                 ...all,
                 [p]: mapProceduresToProps[p](dispatch),
             }),
             {}
-        ),
-    });
+        )
+    );
+
+function buildMapDispatchToProps(mapDispatchToProps, mapProceduresToProps) {
+    return () =>
+        createSelector(
+            [buildGetOwnDispatchProps(mapDispatchToProps), buildGetDispatchProcedureProps(mapProceduresToProps)],
+            (ownDisps, procDisps) => ({
+                ...ownDisps,
+                ...procDisps,
+            })
+        );
 }
 
 function buildMergeProps(mergeProps, mapProceduresToProps) {
